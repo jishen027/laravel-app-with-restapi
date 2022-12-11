3ad43a211712bfc53a2a643c365f72388820990a
JebLee <jishen027@gmail.com>
2022-12-11 21:23:24 +0000

    feat: add sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('products/{id}', [ProductController::class, 'show']);
+// Route::get('products', [ProductController::class, 'index']);
+// Route::post('products', [ProductController::class, 'store']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware'=> ['auth:sanctum']], function(){
+    Route::resource('products', ProductController::class);
 });
